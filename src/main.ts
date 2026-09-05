99c58512367c0fa48768d195471afc280c6d387d
TahReEm <tahreemhossain0@gmail.com>
2026-09-05 06:43:13 -0400

    added frontend cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 5000);
+const port = process.env.PORT || 5000;
+
+await app.listen(port, '0.0.0.0');
 }
 bootstrap();
